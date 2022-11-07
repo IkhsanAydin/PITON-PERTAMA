4fda66950fe4c4a9dc9391b162e6818ce8514678
IkhsanAydin <iksanaydinzahid@gmail.com>
2022-11-07 10:01:24 +0700

    PITON

---
+++ uler.py
@@ -0,0 +1,14 @@
+print("Hello World to Python")
+print(10+10)
+print(30-10)
+print(2*10)
+print(100//5)
+
+a = 20
+b = 20
+print(a//b)
+print(a/b)
+
+usia = 17
+kata = "Usiaku :"
+print (kata, usia)
